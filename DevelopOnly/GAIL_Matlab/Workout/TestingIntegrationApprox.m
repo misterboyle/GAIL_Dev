850d71b4985173c035d05eee380477643e0c9c18
Fred J. Hickernell <hickernell@iit.edu>
2015-06-16 10:44:40 -0500

    Fred's revisions

--- DevelopOnly/GAIL_Matlab/Workout/TestingIntegrationApprox.m
+++ DevelopOnly/GAIL_Matlab/Workout/TestingIntegrationApprox.m
@@ -1,14 +1,41 @@
-% testing different routines for integration approximation
+%% Testing different routines for integration and function approximation
+
+%% Initialization
 format compact
 
 %% Test Functions
+% These are some test functions that we use to demonstrate the strengths
+% and the weaknesses of the integration and function approximation
+% routines.
+
 %f=@(x) x; exactinteg=1/2;
 f=@(x) x.*x; exactinteg=1/3;
 
 %% Integration routines
+% Let's integrate the test function, i.e., we will compute
+%
+% \[ \int_0^1 f(x) \, {\rm d} x. \]
+%
+% The first method is MATLAB's built-in integration routine, which is based
+% on L. F. Shampine, Vectorized Adaptive Quadrature in MATLAB, _Journal of
+% Computational and Applied Mathematics_, *211*, 2008, pp. 131-140.
+
 integ1=integral (f,0,1); err1 = abs(exactinteg-integ1) % matlab built-in
+
+%%
+% The second method is the GAIL routine that has a theoretical
+% justification
+
 integ2=integral_g (f,0,1,1e-12); err2 = abs(exactinteg-integ2) % gail routine
+%%
+% The third method is an improvement on the GAIL routine, which was
+% proposed by
+
 integ3=integralNoPenalty_g (f,0,1,1e-12); err3 = abs(exactinteg-integ3) % fred's routine
+
+%%
+% The fourth method is the Chebfun MATLAB toolbox
+
 fcheb = chebfun(f,[0 1]); integ4=sum(fcheb); err4 = abs(exactinteg-integ4) % chebfun routine
 
 %% Function Approx routines
