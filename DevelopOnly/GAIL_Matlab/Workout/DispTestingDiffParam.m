b7d152180d675521b1d693a489099e56ceadc92d
Fred J. Hickernell <hickernell@iit.edu>
2015-06-29 13:06:01 -0500

    Have orange for not so out of alignment

--- DevelopOnly/GAIL_Matlab/Workout/DispTestingDiffParam.m
+++ DevelopOnly/GAIL_Matlab/Workout/DispTestingDiffParam.m
@@ -19,13 +19,14 @@ pause
 disp(' ')
 disp('<strong>Errors of Integration Methods</strong>')
 disp('MATLAB''s integral')
-disponematrix(err1,tvec,hvec)
+erroralert = @(x) max(0,(abs(x)-abstol)/abstol);
+disponematrix(err1,tvec,hvec,erroralert)
 disp('Chebfun')
-disponematrix(err2,tvec,hvec)
+disponematrix(err2,tvec,hvec,erroralert)
 disp('integral_g (GAIL)')
-disponematrix(err3,tvec,hvec)
+disponematrix(err3,tvec,hvec,erroralert)
 disp('integralNoPenalty_g (GAIL)')
-disponematrix(err4,tvec,hvec)
+disponematrix(err4,tvec,hvec,erroralert)
 pause
 disp(' ')
 disp('<strong>Times of Integration Methods</strong>')
@@ -41,18 +42,18 @@ pause
 disp(' ')
 disp('<strong>Number of Points Needed for Integration Methods</strong>')
 disp('integral_g (GAIL)')
-disponematrix(Npoints3,tvec,hvec)
+disponematrix(Npoints3,tvec,hvec,@(x) abs(x)>0.9*out.nmax)
 disp('integralNoPenalty_g (GAIL)')
-disponematrix(Npoints4,tvec,hvec)
+disponematrix(Npoints4,tvec,hvec,@(x) abs(x)>0.9*out.nmax)
 pause
 
 %% Function Approximation Results
 disp(' ')
 disp('<strong>Errors of Function Approximation Methods</strong>')
 disp('Chebfun')
-disponematrix(errf1,tvec,hvec)
+disponematrix(errf1,tvec,hvec,erroralert)
 disp('funappxglobal_g (GAIL)')
-disponematrix(errf2,tvec,hvec)
+disponematrix(errf2,tvec,hvec,erroralert)
 disp(' ')
 disp('<strong>Times of Function Approximation  Methods</strong>')
 disp('Chebfun')
@@ -63,19 +64,30 @@ pause
 disp(' ')
 disp('<strong>Number of Points Needed for Function Approximation Methods</strong>')
 disp('funappxglobal_g')
-disponematrix(Npointsf2,tvec,hvec)
+disponematrix(Npointsf2,tvec,hvec,@(x) abs(x)>0.9*out.nmax)
 
 end
 
 
-function disponematrix(matrix,tvec,hvec)
+function disponematrix(matrix,tvec,hvec,alertfun)
+if nargin <=3
+   alertfun = @(x) false;
+end
 fprintf(1,'t     | h');
 fprintf(1,'%6.0e',tvec);
 fprintf('\n')
 disp('---------------------------------------------------------------------------')
 for ii = 1:numel(hvec)
    fprintf('%5.0e |  ',hvec(ii))
-   fprintf(1,'%6.0e',matrix(ii,:));
+   for jj = 1:numel(tvec)
+      if alertfun(matrix(ii,jj)) > 10
+         cprintf('red','%6.0e',matrix(ii,jj));
+      elseif alertfun(matrix(ii,jj)) > 1
+         cprintf([1 0.5 0],'%6.0e',matrix(ii,jj));
+      else
+         cprintf('black','%6.0e',matrix(ii,jj));
+      end
+   end
    fprintf('\n')
 end
 disp('===========================================================================')
